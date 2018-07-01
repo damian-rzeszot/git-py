60cc7b59c23a7d5614f4de10f77c7fc1a9355109
Damian Rzeszot <damian.rzeszot@gmail.com>
2018-07-02 00:31:31 +0200

    "help" command enhancements

--- git/commands/help.py
+++ git/commands/help.py
@@ -6,6 +6,7 @@ class HelpCommand(BaseCommand):
     def run(self, *args):
         print("usage:")
         print("  init")
+        print("  add <file>")
         print("  config list")
         print("  remote show")
         print("  remote add <name> <url>")
