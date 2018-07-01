f365d89f3532063d01561c689e3bc9d807edf103
Damian Rzeszot <damian.rzeszot@gmail.com>
2018-07-01 21:09:28 +0200

    validate "remote" subcommands

--- git/commands/remote.py
+++ git/commands/remote.py
@@ -9,12 +9,16 @@ class RemoteCommand(BaseCommand):
             command = args[0]
         except IndexError:
             print("usage:")
-            print("  remote show")
+            print("  remote add <name> <url>")
             exit(0)
 
         try:
             klass = load_command("remote_" + command)
-            klass.run(*args)
+            object = klass(self.repository)
+            object.run(*args[1:])
         except ModuleNotFoundError:
             print("'remote %s' is not a command. See 'help'." % command)
             exit(1)
+        except TypeError:
+            print("'remote %s' is not a correct use. See 'help'." % command)
+            exit(2)
